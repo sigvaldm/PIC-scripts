aa685c65194bcf58b62bb2f99e2fe19f9ac05962
sigvaldm <sigvaldm@fys.uio.no>
2018-02-16 16:11:06 +0100

    Added comparison to OML and estimation of beta

--- plot_IV.py
+++ plot_IV.py
@@ -18,21 +18,19 @@ probe, having one legend. Comparison is made with OML theory.
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 import sys
 import re
 from funcs import *
 
-plt.figure(1)
-plt.grid()
-plt.title('IV-characteristics')
-plt.xlabel('Voltage [V]')
-plt.ylabel('Current [$\mathrm{\mu A/mm}$]')
+def OML_func(V, c, beta):
+    return c*V**beta
 
 paths = sys.argv[1:]
 paths.sort()
 pattern = re.compile('([0-9]+)mm')
-
 datasets = dict()
+
 for path in paths:
     data = readHst(path)
     length = pattern.search(path).group(1)
@@ -48,14 +46,27 @@ for path in paths:
     datasets[length][0].append(V)
     datasets[length][1].append(I)
 
+Voml = np.linspace(0,8,100)
+Ioml = -OML_cylinder(Voml, l=1e-3)*1e6
+plt.plot(Voml, Ioml, label='OML theory')
+
 for key in datasets:
     V = datasets[key][0]
     I = datasets[key][1]
-    plt.plot(V,I,'o',label=key+'mm')
+    popt, pcov = curve_fit(OML_func, V, I)
+    perr = np.sqrt(np.diag(pcov))
+    beta = popt[1]
+    betaerr = 3*perr[1] # 3 sigma error
 
-V = np.linspace(0,8,100)
-I = -OML_cylinder(V, l=1e-3)*1e6
-plt.plot(V, I, label='OML theory')
+    p = plt.plot(V,I,'o',
+                 label=key+'mm, $\\beta=%.2f\pm %.2f$'%(beta,betaerr))
+    color = p[-1].get_color()
 
+    plt.plot(Voml, OML_func(Voml,*popt), '--', color=color, linewidth=1)
+
+plt.title('IV-characteristics')
+plt.xlabel('Voltage [V]')
+plt.ylabel('Current [$\mathrm{\mu A/mm}$]')
 plt.legend(loc='upper left')
+plt.grid()
 plt.show()
