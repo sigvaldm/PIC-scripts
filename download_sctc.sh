1de45178e9ec43316a1afea59c3e7a7b8524c20f
sigvaldm <sigvaldm@fys.uio.no>
2018-03-05 16:06:53 +0100

    Removed username from download script, relying instead on alias

--- download_sctc.sh
+++ download_sctc.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-# Download .hst files from Abel listed in the code:
+# Download latest .hst files from Abel:
 # 
 # 	./download.sh
 # 
-# Downlaod .hst, .topo and .vtk files from Abel:
+# Downlaod latest .hst, .topo and .vtk files from Abel:
 # 
 # 	./download.sh all
 # 
-# The folders on Abel downloaded from are listed below. So is the username.
-# Modify as appropriate. In order not to be asked for password a key-pair must
-# be put in ~/.ssh.
-
-USER="sigvaldm"
+# The folders on Abel downloaded from are listed below.
+#
+# In order for this script to work, the file ~/.ssh/config must have an entry
+# for the alias "abel", e.g.:
+#
+# 	Host abel
+# 		HostName abel.uio.no
+# 		IdentityFile ~/.ssh/id_rsa
+# 		User sigvaldm
+#
+# Where "User" is the appropriate username and "IdentityFile" points to the
+# correct RSA private key. To ensure this works you should be able to log into
+# abel using just "ssh abel" (without necessarily being on the UiO network).
+#
+# If this does not work, make sure you have a copy of your public key (typically
+# named id_rsa.pub) in ~/.ssh on Abel. Make sure all keys has the right
+# permission (chmod 600 <key>). Make sure there's a copy of your public key in
+# ~/.ssh/authorized_keys on Abel. If not, run:
+#
+#	cat <public-key> >> ~/.ssh/authorized_keys
+#
+# Note: If they private key is secured by a passphrase, you will be asked for a
+# passphrase when using ssh. That happens many, many times during this script
+# and will be ennoying. The risk in turning off the passphrase is that someone
+# might steal your computer, or hack into it and steal your private key. But the
+# established connection is not less safe without the passphrase. To turn off
+# the passphrase:
+#	
+#	ssh-keygen -p -f <private-key>
 
 for RUN in \
 	/usit/abel/u1/sigvaldm/PTetra/Projects/SCTC/Run1_50mm_1.0V \
@@ -36,13 +60,13 @@ do
 	if [ "$1" = "all" ]; then
 
 		echo "Downloading latest .hst, .vtk and .topo files for $TARGET"
-		TOP=`ssh $USER@abel.uio.no "ls -t $RUN/pictetra*.topo | head -n 1"`
-		VTK=`ssh $USER@abel.uio.no "ls -t $RUN/pictetra*.vtk | head -n 1"`
-		SCC=`ssh $USER@abel.uio.no "ls -t $RUN/scc*.vtk | head -n 1"`
-		rsync -P $USER@abel.uio.no:$HST $USER@abel.uio.no:$TOP $USER@abel.uio.no:$VTK $USER@abel.uio.no:$SCC $TARGET/
+		TOP=`ssh abel "ls -t $RUN/pictetra*.topo | head -n 1"`
+		VTK=`ssh abel "ls -t $RUN/pictetra*.vtk | head -n 1"`
+		SCC=`ssh abel "ls -t $RUN/scc*.vtk | head -n 1"`
+		rsync -P abel:$HST $USER@abel.uio.no:$TOP $USER@abel.uio.no:$VTK $USER@abel.uio.no:$SCC $TARGET/
 
 	else
 		echo "Downloading latest .hst files for $TARGET"
-		rsync -P $USER@abel.uio.no:$HST $TARGET/
+		rsync -P abel:$HST $TARGET/
 	fi
 done
