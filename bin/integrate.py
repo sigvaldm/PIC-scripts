319f836b3c334e7e934427344936dccf6fdb1cb9
sigvaldm <sigvaldm@fys.uio.no>
2018-11-27 15:54:33 -0700

    Changed integrate.py to return text-file and work on points with arbitrary theta

--- bin/integrate.py
+++ bin/integrate.py
@@ -22,19 +22,28 @@ I_OML = OML_current(Cylinder(r, 1), elec, eta=eta)
 print("I_OML={} A/m".format(I_OML))
 vrms = elec.vth*np.sqrt(2)
 
+xs = []
+ys = []
 zs = []
+thetas = []
 with open(os.path.join(path,'scanpositions.txt')) as file:
     for line in file:
         try:
             x,y,z,theta = line.split()
+            xs.append(float(x))
+            ys.append(float(y))
             zs.append(float(z))
+            thetas.append(float(theta))
         except:
+            # Skip lines that can't be split in the right format.
             pass
 
 zs = np.array(zs)
 Is = np.zeros_like(zs)
 
 for i in range(len(zs)):
+
+    print("Processing point {} of {}".format(i+1, len(zs)))
 
     try:
         mesh = meshio.read(os.path.join(path,'distfunct{:06d}.vtk'.format(i+1)))
@@ -45,7 +54,10 @@ for i in range(len(zs)):
         points *= vrms
         data   /= vrms**3
 
-        data = np.array([-p[0]*d for p, d in zip(points,data)])
+        # data = np.array([-v[0]*f for v, f in zip(points,data)])
+        normal = -np.array([xs[i], ys[i], 0])
+        normal /= np.linalg.norm(normal)
+        data = np.array([np.dot(v,normal)*f for v, f in zip(points, data)])
 
         cell_points = np.array([points[c] for c in cells])
         cell_volumes = np.prod(np.max(cell_points,1) - np.min(cell_points,1),1)
@@ -54,19 +66,32 @@ for i in range(len(zs)):
         integral = np.sum(cell_volumes*cell_averages)
         current = 2*np.pi*r*elec.q*integral
         Is[i] = current
+
     except:
+
+        # Skip files that can't be found. This allows integrate.py to be run
+        # before density is finished with all points.
         pass
 
-if len(zs)==1:
-    print("Single current: {} A/m".format(Is[0]))
+with open(os.path.join(path, 'profile.txt'), 'w') as file:
+    file.write("#:xaxis\tz\n")
+    file.write("#:name\tx\ty\tz\ttheta\tOML\tI\n")
+    file.write("#:units\tm\tm\tm\trad\tA/m\tA/m\n")
 
-else:
-    dz = zs[1]-zs[0]
-    I_int = dz*sum(Is)
-    print("OML      {} A".format(I_OML*l))
-    print("Integral {} A".format(I_int))
+    for x, y, z, theta, I in zip(xs, ys, zs, thetas, Is):
+        file.write("{}\t{}\t{}\t{}\t{}\t{}\n"
+                   .format(x, y, z, theta, I_OML, I))
 
-    plt.plot(zs*1e3, Is*1e3)
-    plt.plot(zs*1e3, I_OML*1e3*np.ones_like(zs))
-    plt.grid()
-    plt.show()
+# if len(zs)==1:
+#     print("Single current: {} A/m".format(Is[0]))
+
+# else:
+#     dz = zs[1]-zs[0]
+#     I_int = dz*sum(Is)
+#     print("OML      {} A".format(I_OML*l))
+#     print("Integral {} A".format(I_int))
+
+#     plt.plot(zs*1e3, Is*1e3)
+#     plt.plot(zs*1e3, I_OML*1e3*np.ones_like(zs))
+#     plt.grid()
+#     plt.show()
